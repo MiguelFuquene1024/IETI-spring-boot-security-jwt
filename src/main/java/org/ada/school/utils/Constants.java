3648985b7f8bde793db44dac2f92734c07bdbef8
Miguel Angel Fuquene Arias <migueplay_10@hotmail.com>
2021-09-14 19:48:25 -0500

    Parte 1 y 2

---
+++ src/main/java/org/ada/school/utils/Constants.java
@@ -0,0 +1,10 @@
+package org.ada.school.utils;
+
+public interface Constants {
+    String CLAIMS_ROLES_KEY="ada_roles";
+    int TOKEN_DURATION_MINUTES = 1440;
+    String COOKIE_NAME="ada-JWT";
+
+    String ADMIN_ROLES="ADMIN";
+    String USER_R0LE="USER";
+}
